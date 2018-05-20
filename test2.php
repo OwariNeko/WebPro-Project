921f0db634b6063da152fa9840989bf4bc7c5fb2
OwariNeko <iceey7@gmail.com>
2018-05-20 16:27:46 +0700

    phptopdfv.2

--- test2.php
+++ test2.php
@@ -10,15 +10,15 @@ require_once __DIR__ . '/vendor/autoload.php';?>
 <?php
 	$mpdf = new \Mpdf\Mpdf();
 	$mpdf->autoScriptToLang = true;
-$mpdf->baseScript = 1;
-$mpdf->autoVietnamese = true;
-$mpdf->autoArabic = true;
+	$mpdf->baseScript = 1;
+	$mpdf->autoVietnamese = true;
+	$mpdf->autoArabic = true;
 	$mpdf->autoLangToFont = true;
-	$strSQL =  "SELECT orderP.orderID, listP.listID, productName, qty,userName , orderP.location, totalPrice, bankName, bankBranch, transferProof, status, transfer.date
-				FROM orderP, listP, transfer, product, users
-				WHERE orderP.orderID = listP.orderID
-				AND   orderP.transferID = transfer.transferID
-				AND   product.productID = listP.productID
+	$strSQL =  "SELECT orderP.orderID, productName,userName , orderP.location, totalPrice, bankName, bankBranch, transferProof, status, transfer.date
+				FROM orderP, transfer, product, users,listP
+				WHERE orderP.transferID = transfer.transferID
+				AND product.productID = listP.productID
+				AND orderP.orderID = listP.orderID
 				AND   users.userID = orderP.userID
 				AND   transfer.status = 'จ่ายแล้ว'
 				"; 
@@ -28,30 +28,54 @@ $mpdf->autoArabic = true;
 	// $result = mysql_query($strSQL);
 	$total = $result->num_rows;
 	 
-		  $mpdf->WriteHTML("ค้นหาพบทั้งสิ้น ".$total." รายการ<br><br>") ;
+		  $mpdf->WriteHTML("ค้นหาพบทั้งสิ้น ".$total." รายการ<br>") ;
 		 $orderID='';
+		 $listTotalPrice='';
          while($row = $result->fetch_assoc()){
 			 $count += 1;
-			 
+			 $userName=$row['userName'];
+			 $totalPrice=$row['totalPrice'];
+			 $location=$row['location'];
 			 if($orderID=='' or $orderID <> $row['orderID']){
-				$orderID = $row['orderID'];
+				 
+				$orderID=$row['orderID'];
+				$strSQL2 ="SELECT listID,productName,productprice
+							FROM listP,product
+							WHERE product.productID = listP.productID
+							AND orderID=('".$orderID."')";
+				$result2 = $conn->query($strSQL2);
 				if($count == '1'){
-					$mpdf->WriteHTML( "<ol>".$orderID) ;
+					$mpdf->WriteHTML( "<ol>".$orderID."	".$userName);
+					$mpdf->WriteHTML("ที่อยู่	".$location);
+								 while($row2 = $result2->fetch_assoc())
+			 {
+			 		$productName =$row2['productName'];
+			 		$productprice=$row2['productprice'];
+					$mpdf->WriteHTML('<li>'.$row2['listID']."	ชื่อสินค้า".$productName."							ราคา	".$productprice.'	บาท</li>');  
+			}
+			$mpdf->WriteHTML("	ราคารวม	".$totalPrice."	บาท") ;
+			$mpdf->WriteHTML("--------------------------------------------------------------------------------------------------------------------------------------------------------");
+				
+					
 				}
 				else{
-					$mpdf->WriteHTML("</ol><ol>".$orderID) ;
+					$mpdf->WriteHTML("</ol><ol>".$orderID."	".$userName) ;
+					$mpdf->WriteHTML("ที่อยู่	".$location);
+								 while($row2 = $result2->fetch_assoc())
+			 {
+			 		$productName =$row2['productName'];
+			 		$productprice=$row2['productprice'];
+					$mpdf->WriteHTML('<li>'.$row2['listID']."	ชื่อสินค้า".$productName."							ราคา	".$productprice.'	บาท</li>');  
+			}
+			$mpdf->WriteHTML("	ราคารวม	".$totalPrice."	บาท") ;
+			$mpdf->WriteHTML("--------------------------------------------------------------------------------------------------------------------------------------------------------");
 				}
 			 }
-			
-			 
-             $mpdf->WriteHTML('<li>'.$row['listID'].'</li>');
-			
-		 }
+
+		}
+		 
 	
 	$mpdf->Output();
-
-
-
 ?>			
 
 	
